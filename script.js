611b2419a95c77ff79f67ef77efc2573c7bc42f1
inclined4X <jezemiahsam48@gmail.com>
2024-03-08 11:08:15 +0000

    concat

--- script.js
+++ script.js
@@ -98,3 +98,7 @@ arr = ['a', 'b', 'c', 'd', 'e'];
 const arr2 = ['j', 'i', 'h', 'g', 'f'];
 console.log(arr2.reverse());
 console.log(arr2);
+
+// concat
+const letters = arr.concat(arr2);
+console.log(letters);
