335539994dada257d9be6c69c94ec651663843a1
inclined4X <jezemiahsam48@gmail.com>
2024-03-08 13:14:17 +0000

    for of vs for each

--- script.js
+++ script.js
@@ -71,8 +71,9 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+/*
 /////////////////////////////////////////////////
 
 let arr = ['a', 'b', 'c', 'd', 'e'];
@@ -106,3 +107,25 @@ console.log(letters);
 //or
 const letters2 = [...arr, ...arr2];
 console.log(letters2);
+
+// JOIN
+console.log(letters2.join(' - '));
+*/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+movements.forEach(function (movement) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+});
