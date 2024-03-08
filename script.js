f0b52bf136f0a2e3d7ac1f3a78da5663cfb49f2b
inclined4X <jezemiahsam48@gmail.com>
2024-03-08 13:33:31 +0000

    right

--- script.js
+++ script.js
@@ -114,18 +114,23 @@ console.log(letters2.join(' - '));
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-for (const movement of movements) {
+//for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
   if (movement > 0) {
-    console.log(`You deposited ${movement}`);
+    console.log(`Movement ${i + 1} You deposited ${movement}`);
   } else {
-    console.log(`You withdrew ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1} You withdrew ${Math.abs(movement)}`);
   }
 }
 
-movements.forEach(function (movement) {
+console.log('--- FOR EACH ---');
+movements.forEach(function (movement, i, arr) {
   if (movement > 0) {
-    console.log(`You deposited ${movement}`);
+    console.log(`Movement ${i + 1} You deposited ${movement}`);
   } else {
-    console.log(`You withdrew ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1} You withdrew ${Math.abs(movement)}`);
   }
 });
+
+// 0: FUNCTION(200)
+// 1: FUNCTION(450)
