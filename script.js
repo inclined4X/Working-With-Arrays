da7685722bd69a78cc3c129660c83a5d4e50959b
inclined4X <jezemiahsam48@gmail.com>
2024-03-09 23:16:36 +0000

    template literal and the of the dollar sign symbol

--- script.js
+++ script.js
@@ -65,9 +65,13 @@ const inputClosePin = document.querySelector('.form__input--pin');
 //DOM ELEMENTS
 const displayMovement = function (movements) {
   movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'wthdrawal';
+
     const html = `
     <div class="movements__row">
-          <div class="movements__type movements__type--deposit">2 deposit</div>
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
           <div class="movements__value">${mov}</div>
         </div>
     `;
