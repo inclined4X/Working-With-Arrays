c4d02e65a305704cee25f3efa8d4718b4435dea3
inclined4X <jezemiahsam48@gmail.com>
2024-03-12 23:35:39 +0000

    map vs for of loop

--- script.js
+++ script.js
@@ -221,9 +221,16 @@ checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 const eurToUsd = 1.1;
 
 const movementsUSD = movements.map(function (mov) {
-  // return mov * 1.1;
-  return 23;
+  return mov * 1.1;
+  // return 23;
 });
 
 console.log(movements);
 console.log(movementsUSD);
+
+const movementsUSDFor = [];
+for (const mov of movements) {
+  movementsUSDFor.push(mov * eurToUsd);
+}
+
+console.log(movementsUSDFor);
