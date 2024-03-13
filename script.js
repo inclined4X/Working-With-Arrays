61c42734757197c7ebff007a698b286e687ccf07
inclined4X <jezemiahsam48@gmail.com>
2024-03-13 17:56:27 +0000

    filter for the withdrawals

--- script.js
+++ script.js
@@ -267,3 +267,12 @@ const deposits = movements.filter(function (mov) {
 });
 console.log(movements);
 console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(function (mov) {
+  return mov < 0;
+});
+console.log(withdrawals);
